da709ab5adca4940b02499a3bc34df758416c8a6
123sunny1 <76585860+123sunny1@users.noreply.github.com>
2021-10-22 20:03:39 +0900

    storeFarm info page update

--- 2.0/java-script/password.js
+++ 2.0/java-script/password.js
@@ -1,4 +1,3 @@
-<!--
 var goodURL  = "#"  //이곳에 인증이 되었을때 이동할 페이지  입력
     alert("패스워드를 입력하셔야 합니다.")
 
@@ -21,4 +20,3 @@ var password =  prompt("PASSWD 입력","")
         location  = "http://seojin1.kro.kr"    // 실패시 이동 주소      history.back();를 넣어도 됨
     }
 }
-//-->
